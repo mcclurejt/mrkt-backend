ae7f96a221d666b7a6a7788f6775255b4944bf98
mcclurejt <mcclurejt@gmail.com>
2020-09-23 00:21:25 -0500

    Add insertion scripts to create db

---
+++ sql/populate_db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS ticker_data;
+
+USE ticker_data;
+
+SET GLOBAL local_infile = 'ON';
+
+CREATE TABLE IF NOT EXISTS Ticker
+(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR (8) NOT NULL UNIQUE
+);
+
+INSERT INTO Ticker (name) VALUES ("AAPL");
+INSERT INTO Ticker (name) VALUES ("AMZN");
+INSERT INTO Ticker (name) VALUES ("FB");
+INSERT INTO Ticker (name) VALUES ("GOOG");
+INSERT INTO Ticker (name) VALUES ("TSLA");
+
+SOURCE data/daily_adjusted/insert_candles.sql;
