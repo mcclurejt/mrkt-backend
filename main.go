5b8b7d107a48d6f4b58adac24a5d8a4da19cf76f
George Finn <george.finn@sogeti.com>
2020-09-29 16:15:27 -0400

    generated CSVs

--- main.go
+++ main.go
@@ -1,34 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/mcclurejt/mrkt-backend/api"
 	"github.com/mcclurejt/mrkt-backend/encoder"
 )
 
+// nolint
+var (
+	stop             = make(chan os.Signal, 1)
+	exit             = os.Exit
+	stderr io.Writer = os.Stderr
+)
+
 func main() {
+	logger := log.New(stderr, "", log.Lshortfile)
+
+	logger.Println("INITIALIZED")
+
 	tickers := [...]string{"AAPL", "AMZN", "FB", "GOOG", "TSLA"}
 	for _, s := range tickers {
-		fmt.Printf("[%s] : Fetching Data\n", s)
+		logger.Printf("[%s] : Fetching Data\n", s)
 		timeSeries := api.GetMonthlyAdjustedTimeSeries(s)
 
-		fmt.Printf("[%s] : Convert to CSV\n", s)
+		logger.Printf("[%s] : Convert to CSV\n", s)
 		headers := timeSeries.GetHeaders()
-		fmt.Printf("[%s] : Header %v\n", s, headers)
+		logger.Printf("[%s] : Header %v\n", s, headers)
 
 		values := timeSeries.GetValues()
-		fmt.Printf("[%s] : \nValues \n", s, values)
+		logger.Printf("[%s] : Values:\n%v\n", s, values)
 
 		encoder.CSV(s, headers, values)
+		logger.Printf("[%s] : Wrote data to CSV\n", s)
 	}
 
-	c := "btc"
-	cryptoTimeSeries := api.GetCryptoTimeSeries(c)
-	fmt.Printf("%v", cryptoTimeSeries)
-	cryptoHeader := cryptoTimeSeries.GetHeaders()
-	cryptoValues := cryptoTimeSeries.GetValues()
+	coins := [...]string{"btc", "eth"}
+	for _, c := range coins {
+		logger.Printf("[%s] : Fetching Data\n", c)
+		cryptoTimeSeries := api.GetCryptoTimeSeries(c)
 
-	encoder.CSV("BTC", cryptoHeader, cryptoValues)
+		logger.Printf("[%s] : Convert to CSV\n", c)
+		cryptoHeaders := cryptoTimeSeries.GetHeaders()
+		logger.Printf("[%s] : Header : %v\n", c, cryptoHeaders)
 
+		cryptoValues := cryptoTimeSeries.GetValues()
+		logger.Printf("[%s] : \nValues : \n %v\n", c, cryptoValues)
+
+		encoder.CSV(strings.ToUpper(c), cryptoHeaders, cryptoValues)
+		logger.Printf("[%s] : Wrote data to CSV\n", strings.ToUpper(c))
+	}
+
+	logger.Println("COMPLETE")
+
+	exit(0)
 }
